2d497abfc117fe006e3fa2027e51f1a20cfca1d3
jhansarmiento <jhancsn1@gmail.com>
2024-11-04 17:56:30 -0500

    Crea la tarjeta DrinkCard y aplica CSS

--- src/components/DrinkCard.tsx
+++ src/components/DrinkCard.tsx
@@ -8,10 +8,21 @@ type DrinkCardProps = {
 export default function DrinkCard({drink} : DrinkCardProps) {
     // Muestra las tarjetas que contiene la receta para preparar las bebidas
     return (
-        <div>
-            <h2>
-                {drink.strDrink}
-            </h2>
+        <div className="border shadow-lg rounded-xl">
+            <div className="p-6">
+                <img 
+                    src={drink.strDrinkThumb} 
+                    alt={`Image of ${drink.strDrink}`}
+                    className="rounded-lg hover:scale-110 transition-transform"
+                />
+            </div>
+            <div className="p-5">
+                <h2 className="text-2xl truncate font-black">{drink.strDrink}</h2>
+                <button
+                    type="button"
+                    className="bg-orange-400 hover:bg-orange-600 mt-5 w-full p-3 font-bold text-white text-lg uppercase rounded-lg"
+                >Show Recipe</button>
+            </div>
         </div>
     )
 }
--- src/views/IndexPage.tsx
+++ src/views/IndexPage.tsx
@@ -13,14 +13,16 @@ export default function IndexPage() {
     <>
       <h1 className="text-6xl font-extrabold">Recetas</h1>
       {hasDrinks ? (
-        <>
+        <div
+          className="grid grid-cols-1 md:grid-cols-3 2xl:grid-cols-4 gap-10 my-10 p-5"
+        >
           {drinks.drinks.map((drink) => (
             <DrinkCard 
               key={drink.idDrink}
               drink={drink}
             />
           ))}
-        </>
+        </div>
       ) : (
       <p className="my-10 text-center text-2xl">
         Utilice el formulario para buscar recetas
