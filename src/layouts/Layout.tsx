7f83e60a4ad13f9bf22595b3c6a95892bf98378e
jhansarmiento <jhancsn1@gmail.com>
2024-11-07 17:18:12 -0500

    Crea el componente de notification y lo conecta con notificationSlice

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -3,6 +3,7 @@ import Header from "../components/Header";
 import Modal from "../components/Modal";
 import { useEffect } from "react";
 import { useAppStore } from "../stores/useAppStore";
+import Notification from "../components/Notification";
 
 export default function Layout() {
 
@@ -20,6 +21,7 @@ export default function Layout() {
           <Outlet />
         </main>
         <Modal />
+        <Notification />
     </>
   );
 }
